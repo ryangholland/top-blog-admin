fcf8a5f4f9dd38ac302fd06d3a25cbddd801b151
ryangholland <rghcode@gmail.com>
2025-03-22 10:03:10 -0400

    Create static header component with basic styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { Navbar, Container, Dropdown } from "react-bootstrap";
+import { GearFill } from "react-bootstrap-icons";
+
+export default function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" className="px-3">
+      <Container fluid>
+        {/* Left: Page Title */}
+        <Navbar.Brand className="fs-2">Home</Navbar.Brand>
+
+        {/* Right: Settings Icon & User Dropdown */}
+        <div className="d-flex align-items-center">
+          {/* Settings Icon */}
+          <GearFill size={20} className="text-light me-3" role="button" />
+
+          {/* User Dropdown */}
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="dark"
+              className="text-light border-0"
+              id="user-dropdown"
+            >
+              Hello, ryan!
+            </Dropdown.Toggle>
+            <Dropdown.Menu align="end">
+              <Dropdown.Item>Log Out</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </Container>
+    </Navbar>
+  );
+}
