564629d1a85387ab1105354388296c9a21468868
ryangholland <rghcode@gmail.com>
2025-03-23 14:38:43 -0400

    Set up router and main page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Layout from "./layouts/Layout";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import Tags from "./pages/Tags";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
     <Layout>
-      <h1>Hello world...</h1>
+      <Router>
+        <Routes>
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/tags" element={<Tags />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </Layout>
   );
 }
